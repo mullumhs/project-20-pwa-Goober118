0517e798caf31d1f837da4e36f9dba574bb17b42
Goober118 <140124210+Goober118@users.noreply.github.com>
2024-11-20 10:40:29 +1100

    Created database model

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+class Game(db.Model)
+    id = db.Collumn(db.integer, primary_key = True)
+    title = db. Collumn(db.string(200), Nullable = False)
+    publisher = db.Collumn(db.string(100), Nullable = True)
+    rating = db.Collumn(db.float, Nullable = True)
+    genre = db.Collumn(db.string, Nullable = True)
